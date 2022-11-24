f9b38298226f3afb3763cd3a7fe8f24cc0df515f
Ewelina Gajewska <50328193+Ewelina04@users.noreply.github.com>
2022-11-24 11:08:18 +0100

    comment out form

--- app.py
+++ app.py
@@ -536,17 +536,17 @@ with st.sidebar:
     contents_radio2 = st.radio("Wybierz leksykon", ("EMOTION MEANINGS", "NAWL", "EMEAN-NAWL"))
 
     #alternative 
-    form = st.form("my_form")
-    form.write("**Wybierz korpus**")
-    box_testowy = form.checkbox("Testowy korpus", value=False)
-    box_txt_input = form.checkbox("Wprowadź tekst", value=False)
-    txt_input = form.txt_input("Twój tekst", "Oczywiście ze Pan Prezydent to nasza duma narodowa!!")
-    if box_testowy:
-        data = load_dataset("Testowy korpus")
-    elif box_txt_input:
-        form.write('\n\n')
-        txt_list = [txt_input]
-        data = pd.DataFrame({'argument': txt_list})
+    #form = st.form("my_form")
+    #form.write("**Wybierz korpus**")
+    #box_testowy = form.checkbox("Testowy korpus", value=False)
+    #box_txt_input = form.checkbox("Wprowadź tekst", value=False)
+    #txt_input = form.txt_input("Twój tekst", "Oczywiście ze Pan Prezydent to nasza duma narodowa!!")
+    #if box_testowy:
+        #data = load_dataset("Testowy korpus")
+    #elif box_txt_input:
+        #form.write('\n\n')
+        #txt_list = [txt_input]
+        #data = pd.DataFrame({'argument': txt_list})
     #add_spacelines(2)
     #form.write('\n\n\n\n\n')
     #contents_radio = form.radio("**Wybierz analizę**", ("Analiza podstawowa", "Analiza szczegółowa"))
