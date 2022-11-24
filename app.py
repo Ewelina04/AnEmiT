5ed50db6a8dc509ced09be3b26a1fc3b4fd73cda
Ewelina Gajewska <50328193+Ewelina04@users.noreply.github.com>
2022-11-24 19:00:30 +0100

    normal pipeline
    pipeline

--- app.py
+++ app.py
@@ -669,15 +669,12 @@ if (box_testowy or box_txt_input) and analise_txt and contents_radio == "Metoda 
         )
 
 elif (box_testowy or box_txt_input) and analise_txt and contents_radio == "Deep learning model":
-    @st.cache
-    def load_pipe(model_path = model_path):
-        sentiment_pipe = pipeline(task = "sentiment-analysis", model = model_path, tokenizer = model_path)
-        return sentiment_pipe
-    sentiment_task = load_pipe()
+    sentiment_task = pipeline(task = "sentiment-analysis", model = model_path, tokenizer = model_path)
     my_data = data.copy()
     if box_testowy:
         my_data = my_data.sample(n=100)
     my_data = my_data.reset_index(drop=True)
+    my_data = my_data[my_data.argument.str.split().map(len) > 1]
     st.write("#### Analiza w toku ...")
 
     my_bar = st.progress(0)
